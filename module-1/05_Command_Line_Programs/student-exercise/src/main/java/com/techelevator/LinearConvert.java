803f7be5e6e8b0d703bc74271c079f70bb22efc1
RJ <rjmikolaj@gmail.com>
2019-05-21 00:30:03 -0400

    linear converter saved

--- module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/src/main/java/com/techelevator/LinearConvert.java
@@ -1,8 +1,20 @@
 package com.techelevator;
-
+import java.util.Scanner;
 public class LinearConvert {
 
 	public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter a length in Meters: ");
+			float meters = (int) input.nextInt();
+			float feet = (meters * (float) 3.280899);
+			System.out.println( "Your length in feet is: " + feet);
+			
+
+		Scanner newInput = new Scanner(System.in);
+		System.out.print("Enter a length in feet: ");
+		float lf = (int) input.nextInt();
+		float lm = (lf * (float) 0.3048);
+		System.out.println("Your length in meters is: " + lm);
 
 	}
 
