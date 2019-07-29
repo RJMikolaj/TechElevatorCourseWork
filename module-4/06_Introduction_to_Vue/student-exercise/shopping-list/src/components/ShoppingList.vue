a6d07f9ffb86c2e111b28e70f999dc87269a800a
RJ <rjmikolaj@gmail.com>
2019-07-29 00:48:34 -0400

    exercise complete

---
+++ module-4/06_Introduction_to_Vue/student-exercise/shopping-list/src/components/ShoppingList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="shopping-list">
+    <h1>RJ's Shopping List</h1>
+    <ul>
+        <li>Quinoa</li>
+        <li>Gogurt</li>
+        <li>Doublemint Gum</li>
+        <li>Almond Milk</li>
+        <li>Carb-heavy impulse buys</li>
+        <li>Food for stress-eating brought on by coding</li>
+    </ul>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.shopping-list {
+    width:450px;
+    background: #fff;
+    margin: 50px auto;
+    font-family: 'Roboto Condensed', sans-serif;
+    border-radius: 10px;
+}
+h1 {
+    background:#f2f2f2;
+    color:#5c8578;
+    padding:10px;
+    font-size:24px;
+    text-transform: uppercase;
+    text-align: center;
+    margin-bottom: 0px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+}
+ul {
+    list-style-type: none;
+    margin:0px;
+    padding:0px;
+}
+li {
+    font-size: 24px;
+    border-bottom:1px solid #f2f2f2;
+    padding:10px 20px;
+}
+li:last-child{
+    border:0px;
+    text-decoration: line-through;
+    color:darkgrey;
+}
+
+li:first-child{
+  text-decoration: line-through;
+  color:darkgrey;
+}
+
+li.completed {
+    text-decoration: line-through;
+    color:darkgrey;
+}
+</style>
